83e69b82f4c4e1076279bd3ccbcfae251b39fb9f
Joel Lacerda <joellacerdaol@gmail.com>
2023-03-03 00:09:07 -0300

    initial commit

---
+++ src/FabricaCarros.java
@@ -0,0 +1,28 @@
+// Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte forma: 
+// - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next";
+// - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.  
+
+import java.util.*;
+
+public class FabricaCarros
+{
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int custoFabrica = scan.nextInt();
+        int porcentagemDistribuidor = scan.nextInt();
+        int PercentualImpostos = scan.nextInt();
+
+        int custoConsumidor;
+
+        int Distribuidor;
+        int ValorImpostos;
+
+        Distribuidor = (custoFabrica * porcentagemDistribuidor) / 100;
+        ValorImpostos = (custoFabrica * PercentualImpostos) / 100;
+        custoConsumidor = custoFabrica + Distribuidor + ValorImpostos;
+
+        System.out.println(custoConsumidor);
+    }
+}
